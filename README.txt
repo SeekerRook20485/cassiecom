e5b66e1eb22353d4d6abd42800865116eb604c81
SeekerRook20485 <68130661+SeekerRook20485@users.noreply.github.com>
2020-07-11 01:35:51 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,32 @@
+********____CassieCom____********
+
+Welcome to Cassiecom !!!
+
+Thank you for using this Program
+Here are some useful tips :
+1. To run the app open the cassiecom.exe file.
+2. The App is Portable but you should copy the whole folder for it to work in other computers.
+3. You never move the files in other folders, delete or rename them.
+4. You can customise the card details in the data.txt but you must always do this with caution.
+   If you are not 100% sure how to do it you should NEVER try ar else the app may not work.
+5. For any bugs or recomendations please feel dree to conduct me at xrist.vardakis@gmail.com
+6. The controls are pretty straightforward but can be a little challenging to master.
+   you can always type help while runing the program or open the help file.
+
+Thats all!
+
+Have fun !!
+
+
+
+
+
+
+
+
+
+
+
+
+Creator : Christopher Vardakis 
+Last Edited: 7/7/2020.
---
+++ source.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+ifstream dat;
+string filename = "shortdata.txt";
+string title = "********____CassieCom____********\n\n";
+
+void open(ifstream &file,string path){
+    file.open(path);
+    if(!file.is_open()) cout << "loading database error\n";
+}
+
+void close(ifstream &file){
+    
+    file.close();;
+}
+
+void card (){
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    for (int i = 0; i<9; i++){
+        cout<< 'x';
+    }
+    cout << "\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout << "X";
+    
+    for (int i = 0; i<7; i++){
+        cout << " ";
+    }
+    cout <<"x\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x   C   x";
+    cout <<"\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x  C C  x";
+    cout <<"\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x C   C x";
+    cout <<"\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x  C C  x";
+    cout <<"\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x   C   x";
+    cout << "\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    cout <<"x";
+    for (int i = 0; i<7; i++){
+        cout << " ";
+    }
+    cout << "x";
+    cout <<"\n";
+    for (int i = 0; i<9; i++){
+        cout << " ";
+    }
+    for (int i = 0; i<9; i++){
+        cout<< 'x';
+    }
+
+
+}
+
+string help(){
+    ifstream help;
+    open(help,"HELP.txt");
+    string buffer;
+    string out = title;
+    while(getline(help,buffer)){
+        out = out + buffer+'\n';
+    }
+ 
+    return out;
+}
+
+string select (string key){
+    bool love = false;
+    bool normal = false;
+    bool kurz = false;
+    if (key == "l" || key == "L"|| key == "Love"|| key == "love"){
+        love = true;
+        filename = "datalove.txt";
+    }
+    else if (key == "n" || key == "N"|| key == "Normal" || key == "normal"){
+        normal = true;
+        filename = "data.txt";
+    }
+    else if (key == "s" || key == "S"|| key == "Short" || key == "short"){
+        kurz = true;
+        filename = "shortdata.txt";
+    }
+    
+    string res = "Fail to select";
+    if ((love && filename == "datalove.txt")||(normal && filename == "data.txt")||(kurz && filename == "shortdata.txt")){
+     res = "Selection successful";
+    }
+    return res;
+}
+
+string details(string key){
+    system ("cls");
+    cout << title;
+    open(dat,filename);
+    string buffer;
+    string res ="";
+    
+    
+    while(getline(dat, buffer)){
+        if (buffer == key){
+            while (getline(dat,buffer)){
+                if (buffer != "000")
+                res = res + buffer+'\n';
+                if (buffer == "000") break;
+            }
+            break;
+        }
+        else if(buffer == "end"){
+            res = "Command Not Found\n";
+        }
+    }
+   
+    close(dat);
+    return res;
+}
+
+string handler(string key){
+string res;
+    if (key == "help" || key == "h" || key == "H") {
+        system ("cls");
+        res = help();
+         return res;
+        }
+    else if(key == "exit" || key == "e" || key == "E"){
+        system ("cls");
+        exit(0);
+        }
+    else if (key == "select"|| key == "s" || key == "S"){
+        string key;
+        system ("cls");
+        cout << title <<"Select format : \n normal/n for Normal spread \n love/l for Love spread\n short/s for short description\n\n>>";
+        cin >> key;
+        return select(key);
+    }
+    else return details(key);
+
+}
+int main (){
+    system ("cls");
+    cout << title;
+    cout << "     Welcome to CassieCom !!!\n\n";
+    card();
+    while (true){
+    string key;
+    cout << "\n\nType Command (help/select/exit/card name):\n>> ";
+    cin >> key;
+    cout << key;
+    cin.ignore();
+    cout << handler(key)<<'\n';
+    
+    }
+}
+ 
